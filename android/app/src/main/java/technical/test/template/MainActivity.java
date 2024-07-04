2a5747d1e1771289cca7bd106e0a1469a5791990
francocores <francocores@gmail.com>
2024-07-04 09:11:42 +0200

    chore: project initialization

---
+++ android/app/src/main/java/technical/test/template/MainActivity.java
@@ -0,0 +1,5 @@
+package technical.test.template;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'technical.test.template',
+  appName: 'technical-test-template',
+  webDir: 'www'
+};
+
+export default config;
