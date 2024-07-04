aa4b067d99a99cdd8d32f3bccea51718f914d3ba
francocores <francocores@gmail.com>
2024-07-04 10:16:18 +0200

    chore: example code of capacitor plugin

---
+++ custom-plugins/capacitor-camera-plugin/android/src/main/java/com/capacitor/camera/Camera.java
@@ -0,0 +1,11 @@
+package com.capacitor.camera;
+
+import android.util.Log;
+
+public class Camera {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Camera } from 'custom-plugins/capacitor-camera-plugin/src';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  echo() {
+    Camera.echo({ value: 'Hello World' }).then((res) => {
+      console.log(res);
+      alert(res.value)
+    });
+  }
 }
